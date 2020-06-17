64a13ba1e0ec8c6de2c4a5394897510530148f92
Jim Ancona <jim@anconafamily.com>
2020-06-17 14:02:05 -0400

    Separate DLQ for each input queue

--- deploy/awslambda/deploy-infra.sh
+++ deploy/awslambda/deploy-infra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 ENVIRONMENT_NAME="${ENVIRONMENT_NAME:?}"
-DOMAIN_NAME="${DOMAIN_NAME:?}"
+# DOMAIN_NAME="${DOMAIN_NAME:?}"
 # set -x
 s3_bucket_name="${ENVIRONMENT_NAME}-deploy"
 if aws s3 ls "s3://${s3_bucket_name}" 2>&1 | grep -q 'NoSuchBucket'
@@ -11,7 +11,7 @@ then
 fi
 echo Processing CloudFormation...
 aws cloudformation package --template-file cms-infra.cf.yaml --output-template-file output-cms-infra.cf.yaml --s3-bucket $s3_bucket_name
-aws cloudformation deploy --template-file output-cms-infra.cf.yaml --stack-name "${ENVIRONMENT_NAME}-infra" --parameter-overrides "EnvironmentName=${ENVIRONMENT_NAME}" "DomainName=${DOMAIN_NAME}" --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file output-cms-infra.cf.yaml --stack-name "${ENVIRONMENT_NAME}-infra" --parameter-overrides "EnvironmentName=${ENVIRONMENT_NAME}" --capabilities CAPABILITY_IAM
 # Enable Data API
 aws rds modify-db-cluster --db-cluster-identifier "${ENVIRONMENT_NAME}-cms" --enable-http-endpoint --apply-immediately
 # Get the secret ARNs from CF
